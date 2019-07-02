aa0e93e3148ac041209a6b83dc03c313ea54b1b7
aritrobanerjee93 <aritrobanerjee93@gmail.com>
2019-07-03 00:36:19 +0530

    Added logs to observe- 1) Recycler Instance Creation 2) Adapter constructor 3) Adapter onCreateViewHolder 4) Adapter onBindViewHolder 5) ViewHolder constructor 6) Setting LinearLayoutManager 7) Setting the Adapter

--- app/src/main/java/com/example/realmdemo2/DisplayActivity.java
+++ app/src/main/java/com/example/realmdemo2/DisplayActivity.java
@@ -1,14 +1,16 @@
 package com.example.realmdemo2;
 
+import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
-import android.os.Bundle;
-
 public class DisplayActivity extends AppCompatActivity {
+    private static final String TAG = "Experiment";
     RecyclerView mRecyclerView;
 
     @Override
@@ -16,10 +18,14 @@ public class DisplayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
         mRecyclerView = findViewById(R.id.personRecycler);
+        Log.i(TAG, "onCreate: before adapter creation" + "Also the recyclerView id is " + mRecyclerView.getId());
         Realm realm = Realm.getDefaultInstance();
         RealmResults<Person> personRealmResults = realm.where(Person.class).findAll();
         MyAdapter myAdapter = new MyAdapter(personRealmResults,this);
+        Log.i(TAG, "onCreate: After adapter creation");
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        Log.i(TAG, "onCreate: After linear layout manager creation and setting");
         mRecyclerView.setAdapter(myAdapter);
+        Log.i(TAG, "onCreate: After setAdapter");
     }
 }
--- app/src/main/java/com/example/realmdemo2/MyAdapter.java
+++ app/src/main/java/com/example/realmdemo2/MyAdapter.java
@@ -1,6 +1,7 @@
 package com.example.realmdemo2;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,20 +14,25 @@ import io.realm.RealmResults;
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
     private RealmResults<Person> mPersonRealmResults;
     private Context mContext;
+    private static final String TAG = "Experiment";
+    private int count;
 
     public MyAdapter(RealmResults<Person> persons, Context context){
         mPersonRealmResults = persons;
         mContext = context;
+        count = 0;
     }
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recyclerview_item,parent,false);
+        Log.i(TAG, "onCreateViewHolder: Triggered And Also the Parent Id is " + parent.getId());
         return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Log.i(TAG, "onBindViewHolder: Triggered");
         Person person = mPersonRealmResults.get(position);
         holder.name.setText(person.getName());
         holder.email.setText(person.getEmail());
@@ -46,6 +52,8 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
         private TextView age;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            count++;
+            Log.i(TAG, "MyViewHolder: Number of Active ViewHolders:" + count);
             name = itemView.findViewById(R.id.name_tv);
             email = itemView.findViewById(R.id.email_tv);
             phone = itemView.findViewById(R.id.phone_tv);
